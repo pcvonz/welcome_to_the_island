31f22e28b19996b3927060125a6fc02db0c6df03
bradkraeling <47050690+bradkraeling@users.noreply.github.com>
2019-01-26 13:59:16 -0800

    ITS ALIVE - dialog

--- UI/DialogueManager.gd
+++ UI/DialogueManager.gd
@@ -7,6 +7,12 @@ var sentenceText
 var counter
 
 var continueButton
+var yesButton
+var noButton
+
+var continueButtonRec
+var yesButtonRec
+var noButtonRec
 
 
 var sentenceArray = []
@@ -22,7 +28,17 @@ func _ready():
 	nameText = get_node("ColorRect/name_border/NameText")
 	sentenceText = get_node("ColorRect/SentenceText")
 	continueButton = get_node("ColorRect/ContinueRec/continue")
+	
+	continueButtonRec = get_node("ColorRect/ContinueRec")
+	yesButtonRec = get_node("ColorRect/Option1Rec")
+	noButtonRec = get_node("ColorRect/Option2Rec")
+	
+	yesButton = get_node("ColorRect/Option1Rec/option1")
+	noButton = get_node("ColorRect/Option2Rec/option2")
+	
 	continueButton.connect("button_down", self, "getNextSentence")
+	yesButton.connect("button_down", self, "getNextSentence")
+	noButton.connect("button_down", self, "getNextSentence")
 	
 	dialogHolder = get_node("DialogHolder")
 	
@@ -43,6 +59,15 @@ func startDialogue():
 	
 func getNextSentence():
 	if counter < sentenceArray.size():
+		var targetSentence = sentenceArray[counter]
+		if "++" in targetSentence:
+			yesButtonRec.show()
+			noButtonRec.show()
+			continueButtonRec.hide()
+		else:
+			yesButtonRec.hide()
+			noButtonRec.hide()
+			continueButtonRec.show()
 		sentenceText.bbcode_text = "[center] " + sentenceArray[counter] + " [/center]"
 		counter = counter +1
 	else:
