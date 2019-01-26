a3add8e95f5e0368485fc554eae0e0a093b2e255
pacificyost <pacificyost@gmail.com>
2019-01-26 10:43:29 -0800

    Json script file loading
    Function to load the script in JSON format into a dictionary.

--- NodeScenes/DecisionMaster.gd
+++ NodeScenes/DecisionMaster.gd
@@ -1,8 +1,18 @@
 extends Node
 
+export(String) var scriptFile
+var script
 var decisions
 
 func _ready():
-	decisions = get_children()
-	print(decisions[0].question)
+	#decisions = get_children()
+	#print(decisions[0].question)
+	LoadScript()
+	print(script)
 
+func LoadScript():
+	var file = File.new()
+	file.open(scriptFile, File.READ)
+	var scriptData = file.get_as_text()
+	file.close()
+	script = parse_json(scriptData)
