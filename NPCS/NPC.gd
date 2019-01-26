deaaaa4af92946db19a1e219f3e4dda2d2530a9a
Paul <pcvonz@gmail.com>
2019-01-26 14:03:58 -0800

    Add random walk to npc

--- NPCS/NPC.gd
+++ NPCS/NPC.gd
@@ -5,31 +5,31 @@ extends "res://CharacterBase/MovingEntity.gd"
 # var b = "textvar"
 onready var map = get_node("../Navigation2D") 
 var path = []
+
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
+	$Node2D/Timer.connect("timeout", self, "_go_to_random_pos")
+
+func _go_to_random_pos():
+	if path.size() == 0:
+		randomize()
+		var cells = map.get_node("TileMap").get_used_cells()
+		var rand_cell = randi() % cells.size() # returns random number between 1 and 100
+		var rand_pos = map.get_node("TileMap").map_to_world(cells[rand_cell])
+		rand_pos = map.get_closest_point(rand_pos)
+		path = map.get_simple_path(self.position, rand_pos)
 
 func _process(delta):
-	while path.size() > 0:
+	if path.size() > 0:
 		var distance_to = self.position.distance_to(path[0])
 		print(distance_to)
 		if self.position.distance_to(path[0]) > 2:
-			var vec_to = self.position - path[0]
+			var vec_to = path[0] - self.position
 			vec_to = vec_to.normalized() * MAX_SPEED
-			print(vec_to)
 			move_and_collide(vec_to)
 		else:
 			path.remove(0)
-		
+
 func _input(event):
 	if event.is_action_pressed('click'):
 		path = map.get_simple_path(self.position, event.position)
 		print(path)
-		
-			
-
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
