a3d03526222d5649d664d6d43bb4706a0df48514
Paul <pcvonz@gmail.com>
2019-01-25 23:11:02 -0800

    Add tilemap, character

---
+++ Character/MovingEntity.gd
@@ -0,0 +1,13 @@
+extends KinematicBody2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+  # Called when the node is added to the scene for the first time.
+  # Initialization here
+  pass
+
+func test():
+  print('hey')
