23d905235cda53a27330cd4baafcd8164a6eb3d3
Paul <pcvonz@gmail.com>
2019-01-26 10:45:23 -0800

    Add base class for npcs and characters

--- Character/Character.gd
+++ Character/Character.gd
@@ -1,22 +1,8 @@
-extends KinematicBody2D
+extends "res://CharacterBase/MovingEntity.gd"
 
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
-
-var left = Vector2(-1, 0)
-var right = Vector2(1, 0)
-var down = Vector2(0, 1)
-var up = Vector2(0, -1)
-const SPEED = 10
-export var MAX_SPEED = 4
-onready var character = get_node("Node2D/Viewport/player/Armature")
-
-func _ready():
-  pass
-
-func _input(event):
-	pass
 
 func _process(delta):
 	if Input.is_action_pressed("ui_left"):
