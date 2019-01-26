3530942366f05964007a9e7752af0d5b4b6886c9
Paul <pcvonz@gmail.com>
2019-01-26 13:25:54 -0800

    Update character camera

--- Character/Character.gd
+++ Character/Character.gd
@@ -3,17 +3,32 @@ extends "res://CharacterBase/MovingEntity.gd"
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
+onready var prev_position = self.position
+var rotate = 0
+onready var anim = player.get_node('AnimationPlayer')
+func move_character(speed, rotation):
+	move_and_collide(speed)
+	player.get_node("Position3D").rotation_degrees.y = rotation
+	if not anim.is_playing():
+		anim.play('Armature|mixamo.com|Layer0')
 
 func _process(delta):
+	var direction = Vector2(0, 0)
 	if Input.is_action_pressed("ui_left"):
-		move_and_collide(left*MAX_SPEED)
-		character.rotation_degrees.y = -90
+		direction += left
+		rotate = 90
 	if Input.is_action_pressed("ui_right"):
-		move_and_collide(right*MAX_SPEED)
-		character.rotation_degrees.y = 90
+		rotate = -90
+		direction += right
 	if Input.is_action_pressed("ui_up"):
-		move_and_collide(up*MAX_SPEED)
-		character.rotation_degrees.y = 180
+		rotate = 180
+		direction += up
 	if Input.is_action_pressed("ui_down"):
-		move_and_collide(down*MAX_SPEED)
-		character.rotation_degrees.y = 0
+		rotate = 0
+		direction += down
+	move_character(direction.normalized()*MAX_SPEED, rotate)
+	if self.position == self.prev_position:
+		print("STOP")
+		anim.stop()
+		anim.seek(0)
+	prev_position = self.position
---
+++ CharacterBase/Model.gd
@@ -0,0 +1,10 @@
+extends Spatial
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+export(NodePath) var character
+
+
+
