8251241a09f5327ed7e61d61a27529922519eb2d
Paul <pcvonz@gmail.com>
2019-01-26 21:05:07 -0800

    Fix autload

--- autoload/scene.gd
+++ autoload/scene.gd
@@ -1,5 +1,10 @@
 extends Node2D
 
+var current_scene = null
+
+func _ready():
+        var root = get_tree().get_root()
+        current_scene = root.get_child(root.get_child_count() -1)
 func goto_scene(path):
     # This function will usually be called from a signal callback,
     # or some other function from the running scene.
