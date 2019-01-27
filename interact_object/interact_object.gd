f85940db7d3b4e3d7ecbb03022e8d5c6ffa66040
Paul <pcvonz@gmail.com>
2019-01-27 14:58:22 -0800

    Add fade in and out on ending transition

--- interact_object/interact_object.gd
+++ interact_object/interact_object.gd
@@ -4,12 +4,20 @@ export var interact_text = "Press space to build boat"
 export var objectName = "Boat"
 export(String, FILE) var Scene
 onready var dbox = get_node("/root/Node2D/CanvasLayer/DialogBox")
+onready var anim = get_node("/root/Node2D/CanvasLayer/AnimationPlayer")
+export var anim_string = "fade_out_black"
+
+func switch_scene(a):
+	get_node("/root/scene/").goto_scene(Scene)
 
 func end_game():
 	if dbox.dialogHolder.isLastConversation():
 		dbox.setConversationHead(objectName,"ID2")
 		dbox.startDialogue(objectName)
-		get_node("/root/scene/").goto_scene(Scene)
+		anim.connect("animation_finished", self, 'switch_scene')
+		anim.set_current_animation(anim_string)
+		anim.seek(0)
+		anim.play()
 	else:
 		dbox.setConversationHead(objectName,"ID1")
 		dbox.startDialogue(objectName)
