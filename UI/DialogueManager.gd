e4f6e5b07c644c1b88132519a5d2d4d7773e11f8
pacificyost <pacificyost@gmail.com>
2019-01-26 16:36:58 -0800

    Updated Dialog System

--- UI/DialogueManager.gd
+++ UI/DialogueManager.gd
@@ -7,6 +7,8 @@ var sentenceText
 var counter
 
 var continueButton
+var response1Button
+var response2Button
 
 
 var sentenceArray = []
@@ -22,31 +24,60 @@ func _ready():
 	nameText = get_node("ColorRect/name_border/NameText")
 	sentenceText = get_node("ColorRect/SentenceText")
 	continueButton = get_node("ColorRect/ContinueRec/continue")
-	continueButton.connect("button_down", self, "getNextSentence")
+	continueButton.connect("button_down", self, "getNextSentence", ["NPC1", 0])
+	
+	response1Button = get_node("ColorRect/Option1Rec/option1")
+	response1Button.connect("button_down", self, "getNextSentence", ["NPC1", 0])
+	
+	response2Button = get_node("ColorRect/Option2Rec/option2")
+	response2Button.connect("button_down", self, "getNextSentence", ["NPC1", 1])
 	
 	dialogHolder = get_node("DialogHolder")
 	
-	hide()
+	startDialogue("NPC1")
+	
+	#hide()
 	
 	
 	pass
 	
-func startDialogue():
-	var dialog = dialogHolder.retrieveDialog()
-	sentenceArray.clear()
-	counter = 0
-	var name = dialog.getName()
-	nameText.bbcode_text = "[center] " + name + " [/center]"
-	sentenceArray = dialog.getSentences()
-	getNextSentence()
-	show()
+func startDialogue(name):
+	var dialog = dialogHolder.retrieveDialog(name)
+	if (dialog == null):
+		hide()
+		return
 	
-func getNextSentence():
-	if counter < sentenceArray.size():
-		sentenceText.bbcode_text = "[center] " + sentenceArray[counter] + " [/center]"
-		counter = counter +1
-	else:
-		endDialogue()
+	displayName(name)
+	displaySentence(dialog["Sentence"])
+	displayResponses(dialog["Responses"])
+	show()
+
+func displayName(name):
+	nameText.bbcode_text = "[center] " + name + " [/center]"
+
+func displaySentence(sentence):
+	sentenceText.bbcode_text = "[center] " + sentence + " [/center]"
+
+func displayResponses(responses):
+	match(responses.size()):
+		0:
+			continue
+		1:
+			response1Button.get_parent().hide()
+			response2Button.get_parent().hide()
+			continueButton.get_parent().show()
+		2:
+			response1Button.text = responses[0]
+			response2Button.text = responses[1]
+			response1Button.get_parent().show()
+			response2Button.get_parent().show()
+			continueButton.get_parent().hide()
+		_:
+			print("*******************TOO MANY RESPONSES*******************")
+
+func getNextSentence(name, response):
+	dialogHolder.responseToNPC(name, response)
+	startDialogue(name)
 		
 		
 func endDialogue():
