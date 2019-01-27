994348c5a45c3312e670f1ff40791bd3cd5a1b85
pacificyost <pacificyost@gmail.com>
2019-01-27 14:42:58 -0800

    Sign/Boat dialog

--- interact_object/interact_object.gd
+++ interact_object/interact_object.gd
@@ -1,10 +1,16 @@
 extends Node2D
 
 export var interact_text = "Press space to build boat"
+export var objectName = "Boat"
 export(String, FILE) var Scene
 onready var dbox = get_node("/root/Node2D/CanvasLayer/DialogBox")
 
 func end_game():
 	if dbox.dialogHolder.isLastConversation():
+		dbox.setConversationHead(objectName,"ID2")
+		dbox.startDialogue(objectName)
 		get_node("/root/scene/").goto_scene(Scene)
+	else:
+		dbox.setConversationHead(objectName,"ID1")
+		dbox.startDialogue(objectName)
 	
